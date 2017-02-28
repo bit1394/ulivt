30dc80cc2653b81b446890d920096935b77816cd
bit <bit@192.168.0.3>
2017-02-28 21:32:35 +0300

    ok

--- Htrn_sprn15/src/solver.java
+++ Htrn_sprn15/src/solver.java
@@ -1,6 +1,22 @@
 import java.util.Scanner;
 
 public class solver {
+	/*
+	 H. Длинное слово
+Дана строка, состоящая из строчных латинских букв и пробелов. Определите, сколько букв в самом длинном слове этой строки.
+
+Входные данные
+Ввод содержит строку S, длина которой не превышает 100 символов. Гарантируется, что в строке S есть хотя бы одна буква.
+
+Выходные данные
+Выведите одно целое число — количество букв в самом длинном слове строки.
+
+Примеры
+Входные данные								Выходные данные
+this is an example							7
+this string has not so many long words		6
+programming is cool							11
+	 */
 
 	public static void main(String[] args) {
 		String s;
@@ -23,3 +39,5 @@ public class solver {
 	System.out.println(max);
 	}
 }
+
+//ok
