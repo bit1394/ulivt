a4a76b90d4c2b40ae02f9c6b40b898ef7b94b6cd
bit <bit@192.168.0.3>
2017-03-01 20:34:52 +0300

    ok

--- Jtrn_sprn15/src/solver.java
+++ Jtrn_sprn15/src/solver.java
@@ -1,6 +1,25 @@
 import java.util.Scanner;
 
 public class solver {
+	/*
+J. Вычёркивание
+Дано слово A и слово B. Определите, можно ли получить слово B, вычеркнув в слове A некоторое (возможно, пустое) подмножество букв.
+
+Входные данные
+Первая строка содержит слово A, состоящее из строчных латинских букв. Длина слова A не превышает 100 символов.
+
+Первая строка содержит слово B, состоящее из строчных латинских букв. Длина слова B не превышает 100 символов.
+
+Выходные данные
+Выведите YES, если слово B можно получить вычёркиванием букв из слова A. В противном случае выведите NO.
+
+Примеры
+Входные данные				Выходные данные
+informational
+normal						YES
+informational
+fractional					NO
+	 */
 	public static void main (String[] args){
 		Scanner in = new Scanner (System.in);
 		String a = in.nextLine();
@@ -14,11 +33,11 @@ public class solver {
 		is_eq = false;
 		
 		while (pos < b.length()){
-			sub = b.substring(pos, pos + 1);
+			sub = b.substring(pos, pos + 1); //System.out.println(sub);
 			//System.out.println(sub);
-			if(a.indexOf(sub) >= 0) {
+			if(a.indexOf(sub) >= 0) { //System.out.println(a);
 				is_eq = true;
-				a = a.substring(a.indexOf(sub));
+				a = a.substring(a.indexOf(sub) + 1);
 				pos++;
 				} else {
 					is_eq = false;
@@ -29,3 +48,4 @@ public class solver {
 		System.out.println(answ);
 	}
 }
+//ok
